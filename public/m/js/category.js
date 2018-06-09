b25bb31dab3adf67f9ba0d5294cb4455b0cb6a00
dashige <541841650@qq.com>
2018-06-09 14:45:59 +0800

    添加了登录和注册静态页面

--- public/m/js/category.js
+++ public/m/js/category.js
@@ -5,6 +5,7 @@ $(function () {
     // 通过乐淘对象初始化区域滚动
     letao.initScroll();
     letao.getCategoryLeft();
+    letao.getCategoryRight();
 })
 //Letao的构造函数
 var Letao = function () {
@@ -34,11 +35,47 @@ Letao.prototype = {
     // 获取左侧分类的数据
     getCategoryLeft: function () {
         // 1. 发送请求获取左侧分类的数据
-        // $.ajax({
-        //     url:'category/queryTopCategory',
-        //     success: function (data) {
-        //         console.log(data);
-        //     }
-        // })
+        $.ajax({
+
+            url: '/category/queryTopCategory',
+            success: function (data) {
+                var html = template('cateogoryLeftTmp', data);
+                $(".category-left ul").html(html);
+            }
+        })
+    },
+    // 点击左侧获取右侧品牌数据
+    getCategoryRight: function () {
+        getRightData(1);
+        $('.category-left ul').on("click", 'a', function (e) {
+            // 去类名
+            $(e.target.parentNode).addClass('active').siblings().removeClass('active');
+            // var current = $(e.target);
+            // console.log(current);
+            var id = e.target.dataset['id'];
+            
+            getRightData(id);
+        });
+
+        function getRightData(id) {
+            $.ajax({
+                url: '/category/querySecondCategory',
+                data: {
+                    id: id
+                }, //右侧API需要传递参数
+                success: function (data) {
+                    // 6. 调用右侧分类的模板生成html
+                    var html = template('categoryRightTmp', data);
+                    if (html) {
+                        // 7. 把html渲染到右侧的mui-row里面
+                        $('.category-right .mui-row').html(html);
+                    } else {
+                        // 8. 提示没有数据
+                        $('.category-right .mui-row').html('<h6>真没了</h6>');
+                    }
+                }
+            });
+        }
     }
+
 }
